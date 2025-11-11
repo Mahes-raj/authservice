31941150ca17578815d535b4d5aa614eaefb2686
Mahes-raj <maheswaridevops233022@gmail.com>
2025-11-11 13:50:57 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+import express from "express";
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Auth Service is running successfully!");
+});
+
+export default app;
